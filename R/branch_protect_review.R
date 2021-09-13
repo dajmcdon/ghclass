23bc64e5c6ec8cd1c429e8d886a6c27885071cec
Daniel J. McDonald <dajmcdon@gmail.com>
2021-09-12 21:39:38 -0700

    extra args for branch protection

--- R/branch_protect_review.R
+++ R/branch_protect_review.R
@@ -1,5 +1,20 @@
-github_api_branch_protect_with_review <-function(repo, branch) {
-  body <- '{"required_status_checks":null,"enforce_admins":false,"required_pull_request_reviews":{"dismiss_stale_reviews":false,"require_code_owner_reviews":true,"required_approving_review_count":1},"restrictions":null}'
+github_api_branch_protect_with_review <-function(repo, branch,
+                                                 n_approvals,
+                                                 code_owners,
+                                                 enforce_admins) {
+  body <- paste0(
+    '{',
+    '"required_status_checks":null,',
+    str_glue('"enforce_admins":{enforce_admins},'),
+    '"required_pull_request_reviews"',
+    ':{',
+    '"dismiss_stale_reviews":false,',
+    str_glue('"require_code_owner_reviews":{code_owners},'),
+    str_glue('"required_approving_review_count":{n_approvals}'),
+    '},',
+    '"restrictions":null',
+    '}'
+  )
 
   ghclass_api_v3_req(
     "PUT /repos/:owner/:repo/branches/:branch/protection",
@@ -26,6 +41,10 @@ github_api_branch_protect_with_review <-function(repo, branch) {
 #'
 #' @param repo Github repository address in `owner/repo` format.
 #' @param branch Repository branch to use.
+#' @param n_approvals Integer. Number of required PR reviews. Rounds to nearest
+#'  integer if necessary.
+#' @param code_owners Logical. Must CODEOWNERS reviews be required.
+#' @param enforce_admins Logical. Does protection apply to admins.
 #'
 #'
 #' @return
@@ -39,13 +58,20 @@ github_api_branch_protect_with_review <-function(repo, branch) {
 #'   content = "* @dajmcdon")
 #' branch_protect_with_review("ghclass-test/protect_me", "main")
 #' }
-branch_protect_with_review <- function(repo, branch) {
+branch_protect_with_review <- function(repo, branch,
+                                       n_approvals = 1L,
+                                       code_owners = FALSE,
+                                       enforce_admins = FALSE) {
   arg_is_chr(repo, branch)
+  n_approvals <- as.character(round(n_approvals))
+  code_owners <- tolower(as.character(code_owners))
+  enforce_admins <- tolower(as.character(enforce_admins))
 
   purrr::walk2(
     repo, branch,
     function(repo, branch) {
-      res = purrr::safely(github_api_branch_protect_with_review)(repo, branch)
+      res = purrr::safely(github_api_branch_protect_with_review)(
+        repo, branch, n_approvals, code_owners, enforce_admins)
 
       repo_fmt = format_repo(repo, branch)
 
